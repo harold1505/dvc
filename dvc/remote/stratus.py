a7aaf85feb8f5181e9d096cd6a7ff367c52aed8f
zlabsnlp <harold.v@zohocorp.com>
2020-03-04 18:21:13 +0530

    stratus init removed

--- dvc/remote/stratus.py
+++ dvc/remote/stratus.py
@@ -12,9 +12,3 @@ logger = logging.getLogger(__name__)
 
 class RemoteSTRATUS(RemoteHTTPS):
     scheme = Schemes.STRATUS
-    def __init__(self, repo, config):
-        super().__init__(repo,config)
-        logger.info("initialized successfully")
-
-    def _upload(self, from_file, to_info, name=None, no_progress_bar=False):
-        raise HTTPError(400, "stratus upload not implemented")
