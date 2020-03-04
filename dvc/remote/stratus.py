491da1c53682536c58ee4ff36968650e84a5ae8f
zlabsnlp <harold.v@zohocorp.com>
2020-03-04 15:19:55 +0530

    stratus initialization removed

--- dvc/remote/stratus.py
+++ dvc/remote/stratus.py
@@ -11,8 +11,4 @@ logger = logging.getLogger(__name__)
 
 class RemoteSTRATUS(RemoteHTTP):
     scheme = Schemes.STRATUS
-    def __init__(self, repo, config):
-        super().__init__(repo,config)
-        logger.info("initialized successfully")
-
     
