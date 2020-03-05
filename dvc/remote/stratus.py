b582ca1a7a788e54e5c6963144a4923bef07d4db
zlabsnlp <harold.v@zohocorp.com>
2020-03-05 12:29:04 +0530

    url log

--- dvc/remote/stratus.py
+++ dvc/remote/stratus.py
@@ -14,7 +14,11 @@ class RemoteSTRATUS(RemoteHTTPS):
     
     def __init__(self, repo, config):
         super().__init__(repo, config)
-        
+    
+    def checksum_to_path_info(self, checksum):
+        return self.path_info / checksum
+
     def _upload(self, from_file, to_info, name=None, no_progress_bar=False):
         logger.info("stratus upload method")
         logger.info("url:"+str(to_info.url))
+        
