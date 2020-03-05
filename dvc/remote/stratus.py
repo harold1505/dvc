7197842c22cac3a9fa6687a860d7fcd4ed4fb686
zlabsnlp <harold.v@zohocorp.com>
2020-03-05 13:30:11 +0530

    url configured

--- dvc/remote/stratus.py
+++ dvc/remote/stratus.py
@@ -1,24 +1,35 @@
 import logging
+import threading
 
 
+from funcy import cached_property, memoize, wrap_prop, wrap_with
 
 from .https import RemoteHTTPS
 from dvc.scheme import Schemes
 from dvc.exceptions import HTTPError
+from dvc.prompt import prompt
 
 
 
 logger = logging.getLogger(__name__)
 
+@wrap_with(threading.Lock())
+@memoize
+def ask_password(message):
+    return prompt.password(message)
+
 class RemoteSTRATUS(RemoteHTTPS):
+
     
     def __init__(self, repo, config):
         super().__init__(repo, config)
+        self.bucket_name = self.ask_password('Enter bucket name:')
     
     def checksum_to_path_info(self, checksum):
-        return self.path_info / checksum
+        return self.path_info / 'b'/self.bucket_name / 'k'/checksum
 
     def _upload(self, from_file, to_info, name=None, no_progress_bar=False):
         logger.info("stratus upload method")
         logger.info("url:"+str(to_info.url))
+        
         
