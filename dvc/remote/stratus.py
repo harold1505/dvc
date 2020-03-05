1b16469625592ddc95f787110e819c8e4d03ea96
zlabsnlp <harold.v@zohocorp.com>
2020-03-05 11:46:56 +0530

    error correction

--- dvc/remote/stratus.py
+++ dvc/remote/stratus.py
@@ -2,7 +2,7 @@ import logging
 
 
 
-from .https import RemoteHTTP
+from .https import RemoteHTTPS
 from dvc.scheme import Schemes
 from dvc.exceptions import HTTPError
 
@@ -10,5 +10,5 @@ from dvc.exceptions import HTTPError
 
 logger = logging.getLogger(__name__)
 
-class RemoteSTRATUS(RemoteHTTP):
+class RemoteSTRATUS(RemoteHTTPS):
     pass
